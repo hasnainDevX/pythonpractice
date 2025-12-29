8d60b3f589cd5e447e598bf8680f377c00938c94
codebyte0 <codebyte.wd@gmail.com>
2025-12-30 01:19:42 +0500

    added 2 chaps

---
+++ chap2.py
@@ -0,0 +1,27 @@
+# TASK 1
+a = 5
+b = 10
+print("The sum of a and b is:", a + b)
+
+# TASK 2 
+a = int(input("Enter number: "))
+b = a % 2
+print(b)
+
+# TASK 3
+a = input("Enter number: ")
+print(type(a))
+
+# TASK 4
+a = 34 
+b = 44
+print(a>b)
+
+# TASK 5
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("avg is", (a + b) / 2)
+
+# TASK 6
+a = int(input("Enter number: "))
+print("square of a number is", a ** 2)
