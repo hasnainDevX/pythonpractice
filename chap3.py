87effe7364ccdeaaaea155e45c417fb9d650d804
codebyte0 <codebyte.wd@gmail.com>
2026-01-01 00:51:02 +0500

    data types and opertors

---
+++ chap3.py
@@ -0,0 +1,30 @@
+# import os
+
+# files = os.listdir("/")
+# for i in files:
+#     print(i)
+
+# Operators done 
+# Data Types Done 
+
+# print(True and False)  # False
+# print(True or False)   # True
+# print(not(True))  # False
+# print(3 > 2 and 2 < 3)  # True
+# print(3 > 2 or 2 > 3)   # True
+
+
+# Type Casting
+a = 31
+print(type(a))  # <class 'int'>
+b = 32.1
+print(type(b))  # <class 'float'>
+c = "Hello"
+print(type(c))  # <class 'str'>
+d = complex(3, 4)
+print(type(d))  # <class 'complex'>
+e = float(a)
+print(e)  # 31.0
+print(type(e))  # <class 'float'>
+f = int(b)
+print(f)  # 32
